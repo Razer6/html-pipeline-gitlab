d6985770c08eede759aa584ee9a570037aeeb2a8
Robert Schilling <rschilling@student.tugraz.at>
2014-09-07 13:51:55 +0200

    Use ruby 1.9 hash syntax

--- test/html/pipeline/gitlab_gemoji_filter_test.rb
+++ test/html/pipeline/gitlab_gemoji_filter_test.rb
@@ -5,20 +5,20 @@ class HTML::Pipeline::GitLabEmojiFilterTest < Minitest::Test
   GitLabEmojiFilter = HTML::Pipeline::GitLab::GitLabEmojiFilter
   
   def test_emojify
-    filter = GitLabEmojiFilter.new('<p>:heart:</p>', {:asset_root: 'https://foo.com'})
+    filter = GitLabEmojiFilter.new('<p>:heart:</p>', {asset_root: 'https://foo.com'})
     doc = filter.call
     assert_match 'https://foo.com/emoji/heart.png', doc.search('img').attr('src').value
   end
 
   def test_unsupported_emoji
     block = '<p>:sheep:</p>'
-    filter = GitLabEmojiFilter.new(block, {:asset_root: 'https://foo.com'})
+    filter = GitLabEmojiFilter.new(block, {asset_root: 'https://foo.com'})
     doc = filter.call
     assert_match block, doc.to_html
   end
   
   def test_uri_encoding
-    filter = GitLabEmojiFilter.new('<p>:+1:</p>', {:asset_root: 'https://foo.com'})
+    filter = GitLabEmojiFilter.new('<p>:+1:</p>', {asset_root: 'https://foo.com'})
     doc = filter.call
     assert_match 'https://foo.com/emoji/%2B1.png', doc.search('img').attr('src').value
   end
