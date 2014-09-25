76c3d504930f3ec31b0ced4ee9da2762c2587040
Robert Schilling <rschilling@student.tugraz.at>
2014-09-25 13:35:42 +0200

    Define markdown pipeline

--- lib/html/pipeline/gitlab.rb
+++ lib/html/pipeline/gitlab.rb
@@ -1,2 +1,17 @@
+require 'html/pipeline'
 require 'html/pipeline/gitlab/version'
 require 'html/pipeline/gitlab/gitlab_emoji_filter'
+
+module HTML
+  class Pipeline
+    class Gitlab
+      # GitLab related filters for html-pipeline. Implements new filters used
+      # by GitLab.
+
+      # Most used markdown pipeline in GitLab. Performs emoji replace,,ent
+      MarkdownPipeline = HTML::Pipeline.new [
+        HTML::Pipeline::GitLab::GitLabEmojiFilter
+      ]
+    end
+  end
+end
